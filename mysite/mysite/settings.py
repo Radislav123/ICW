717405919abaf8716de4302c9d1339b14ca73b36
Radislav123 <forgameslegolas@mail.ru>
2019-04-20 17:03:53 +0300

    ICW was started

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'pickle_tax.apps.PickleTaxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ mysite/pickle_tax/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PickleTaxConfig(AppConfig):
+    name = 'pickle_tax'
