54f4e1785fe7bfdd52fb6544a388c9f4fb638f62
Radislav123 <forgameslegolas@mail.ru>
2019-05-17 19:51:42 +0300

    authorization request logging

--- pickletax/databasedaemon/views.py
+++ pickletax/databasedaemon/views.py
@@ -7,10 +7,8 @@ import json
 
 def log_message(request, logger):
 	try:
-		logger.info("try section")
-		logger.debug(request.body.decode("utf-8"))
+		logger.debug("codec - " + request.body.decode("utf-8"))
 	except BaseException as error:
-		logger.info("error section")
 		logger.error(error)
 
 
